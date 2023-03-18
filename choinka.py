69d11a0125e46ee4bb2b05ca1f4e15063a8f47f2
Mateoo_J <73313586+Mateusz7AiR2@users.noreply.github.com>
2023-03-18 21:57:32 +0100

    Add files via upload

---
+++ choinka.py
@@ -0,0 +1,26 @@
+
+def choinka(zakres):
+
+    x = 1
+    for i in range(zakres):
+
+        for z in range(zakres):
+            print((zakres-i-1)*" "+ (x) * "*"+ (zakres-i-1)*" ")
+            x += 2
+            break
+choinka(5)
+
+def holi(n):
+    z=n-1
+    x=1
+    for i in range(0,n):
+        for i in range(0,z):
+            print(' ', end= '')
+        for i in range(0,x):
+            print("+",end='')
+        for i in range(0,z):
+            print(' ',end='')
+        x=x+2
+        z=z-1
+        print()
+holi(5)
